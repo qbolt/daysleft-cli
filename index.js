fe31e146091274b8006e30fda085196530491e3e
Quinton Bolt <qbolt@cooksys.com>
2019-04-13 17:47:19 -0500

    simplified structure; implemented concatenating multiple cli arguments; updated npm vuln packages

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const daysleft = require('daysleft');
+const daysleft = require('daysleft')
+const meow = require('meow')
 
-module.exports = input => {
-	try {
-		return daysleft(input);
-	} catch (err) {
-		return err.message;
-	}
-};
+const cli = meow(`
+	Usage
+	  $ daysleft [input]
+
+	Example1 (assuming today is 12-24-2017
+	  $ daysleft 12-25-2017
+	  1
+`)
+
+module.exports = getDaysLeft = input => {
+  try {
+    return daysleft(input)
+  } catch (err) {
+    return err.message
+  }
+}
+const input = cli.input
+  .join('-')
+  .replace('th', '')
+  .replace('st', '')
+  .replace('nd', '')
+console.log(getDaysLeft(input))
